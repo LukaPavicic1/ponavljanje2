47d21b6c65602e11cc471f45fc03fb95f3c7ad5d
LukaPavicic1 <91119924+LukaPavicic1@users.noreply.github.com>
2022-09-19 07:27:36 +0200

    Add files via upload

---
+++ ponavljanje2/ponavljanje2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ponavljanje2
+{
+    internal class Program
+    {
+        private static string a;
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Upišite znakove: ");
+
+            a = Console.ReadLine();
+
+            string a1;
+            a1 = a.Replace(" ", "_");
+            Console.WriteLine(a1);
+
+            Console.ReadKey();
+
+        }
+    }
+}
